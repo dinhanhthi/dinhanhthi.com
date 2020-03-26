f02e6196df582cf964a8af52d38a573ee3588899
Anh-Thi DINH <dinhanhthi@gmail.com>
2020-03-26 13:21:51 +0100

    test

--- rm_folders.sh
+++ rm_folders.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-echo 'Reset some private folders after merging.'
-git reset _posts _data _drafts img pages _config.yml index.html README.md .gitattributes feed.xml 
+# used for branch notetheme2 only
+echo 'Reset some only-this-branch folders after merging.'
+git reset _data _drafts img pages _config.yml index.html README.md .gitattributes feed.xml 
 git checkout .
-rm -rf tools files
-rm dat.com.code-workspace
 git add .
 git commit -m "update from master"
 
