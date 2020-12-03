41e0f40140eb2f79006615413bd3e1d2c95b8103
Thi <dinhanhthi@gmail.com>
2020-12-03 15:31:03 +0100

    fix #comment js

--- src/main.js
+++ src/main.js
@@ -164,9 +164,12 @@ if (window.ResizeObserver && document.querySelector("header nav #nav")) {
   }
 
   new ResizeObserver(() => {
-    bottom =
+    if (document.querySelector("#comments,footer")){
+      bottom =
       document.scrollingElement.scrollTop +
+      // document.querySelector("#comments,footer").getBoundingClientRect().top;
       document.querySelector("#comments,footer").getBoundingClientRect().top;
+    }
     winHeight = window.innerHeight;
     scroll();
   }).observe(document.body);
