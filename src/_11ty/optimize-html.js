ef8f28e9c8c18c96853537445ba2018e44b17552
Thi <dinhanhthi@gmail.com>
2021-09-29 21:20:06 +0200

    Minify fontello

--- src/_11ty/optimize-html.js
+++ src/_11ty/optimize-html.js
@@ -10,6 +10,7 @@ const csso = require("csso");
 /**
  * Inlines the CSS.
  * Makes font display display-swap
+ * Minify fontello css
  * Minifies and optimizes the JS
  * Optimizes HTML
  * Optimizes AMP
@@ -27,6 +28,30 @@ const minifyCss = async (rawContent, outputPath) => {
       encoding: "utf-8",
     });
     before = before.replace(/@font-face {/g, "@font-face {font-display:swap;");
+    const after = csso.minify(before).css;
+    content = content.replace("</head>", `<style>${after}</style></head>`);
+  }
+  return content;
+};
+
+const minifyFontelloCss = async (rawContent, outputPath) => {
+  let content = rawContent;
+  if (
+    outputPath &&
+    outputPath.endsWith(".html") &&
+    !isAmp(content) &&
+    !/data-style-override/.test(content)
+  ) {
+    let before = require("fs").readFileSync("src/fontello/css/fontello.css", {
+      encoding: "utf-8",
+    });
+    before = before.replace(/@font-face {/g, "@font-face {font-display:swap;");
+
+    // For relative paths in fontello
+    // Note that, in .eleventy.js, there is a
+    // eleventyConfig.addPassthroughCopy({ "src/fontello": "fontello" });
+    before = before.replace(/..\/font\/fontello/g, "fontello/font/fontello");
+
     const after = csso.minify(before).css;
     content = content.replace("</head>", `<style>${after}</style></head>`);
   }
@@ -71,6 +96,7 @@ module.exports = {
     } else {
       eleventyConfig.addTransform("minifyCss", minifyCss);
     }
+    eleventyConfig.addTransform("minifyFontelloCss", minifyFontelloCss);
     eleventyConfig.addTransform("minifyHtml", minifyHtml);
     eleventyConfig.addTransform("optimizeAmp", optimizeAmp);
   },
