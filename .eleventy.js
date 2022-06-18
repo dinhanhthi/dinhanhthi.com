6d5c1a034ec6e8ad0d421ec4a9e3dd799b2fb71e
Thi <dinhanhthi@gmail.com>
2022-06-18 22:14:20 +0200

    Add limitPosts and sortBy options to postslist

--- .eleventy.js
+++ .eleventy.js
@@ -219,9 +219,8 @@ module.exports = function (eleventyConfig) {
 
   // Get the first `n` elements of a collection
   eleventyConfig.addFilter("head", (array, n) => {
-    if (n < 0) {
-      return array.slice(n);
-    }
+    if (!n) return array;
+    if (n < 0) return array.slice(n);
     return array.slice(0, n);
   });
 
