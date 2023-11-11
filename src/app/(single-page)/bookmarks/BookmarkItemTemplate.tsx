4e3905d29d6657b0d7962ca896b8c5ba5d938da8
Thi <dinhanhthi@gmail.com>
2023-11-11 14:14:31 +0100

    chore
    chore
    
    chore

--- src/app/(single-page)/bookmarks/BookmarkItemTemplate.tsx
+++ src/app/(single-page)/bookmarks/BookmarkItemTemplate.tsx
@@ -76,11 +76,15 @@ export default function BookmarkItemTemplate({ mark }: { mark: BookmarkItem }) {
             )}
             {mark.title}
           </div>
-          <div className="text-slate-600 text-[0.85rem] group-hover:text-slate-800">
-            {mark.description}
-          </div>
+          {!!mark.description && !!mark.description?.length && (
+            <div className="text-slate-600 text-[0.85rem] group-hover:text-slate-800">
+              {mark.description}
+            </div>
+          )}
         </div>
-        <div className="text-[0.8rem] italic text-slate-500">{mark.url}</div>
+        <div className="text-[0.8rem] italic text-slate-500 text-ellipsis w-full overflow-hidden">
+          {mark.url}
+        </div>
       </div>
     </a>
   )
@@ -88,10 +92,9 @@ export default function BookmarkItemTemplate({ mark }: { mark: BookmarkItem }) {
 
 const ImagePlaceholder = () => (
   <div
-    style={{ width: 120 }}
     className={cn(
-      'bg-gray-100 flex items-center justify-center animate-pulse rounded-md w-[120px] min-w-[120px]',
-      'h-full'
+      'bg-gray-100 flex items-center justify-center animate-pulse rounded-md w-full',
+      'h-full p-4'
     )}
   >
     <AiOutlineLoading3Quarters className="text-[25px] text-slate-400 animate-spin" />
@@ -103,7 +106,9 @@ export function SkeletonBookmarkItemTemplate() {
     <div className="flex flex-row gap-4 border border-slate-200 p-4 rounded-md bg-white">
       {/* Featured image */}
       <div
-        className={cn('rounded-md w-[120px] min-w-[120px] overflow-hidden border-slate-100 border')}
+        className={cn(
+          'rounded-md w-full sm:w-[100px] min-w-[100px] max-h-[100px] sm:max-h-full overflow-hidden border-slate-100 border'
+        )}
       >
         <div className={cn('p-4', 'flex items-center justify-center bg-slate-100')}>
           <BsFillBookmarkHeartFill className="text-2xl text-slate-300" />
