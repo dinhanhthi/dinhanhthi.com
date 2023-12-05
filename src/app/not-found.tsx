9ffe1a56dc675a9329f892f5a310410be165b42d
Thi <dinhanhthi@gmail.com>
2023-12-05 16:21:01 +0100

    fix: modify 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -23,11 +23,38 @@ export default function NotFoundPage() {
   return (
     <Container className={cn('h-screen', containerNormal)}>
       <div className={cn('flex flex-col gap-6 items-center justify-center w-full h-full')}>
-        <h1 className="text-4xl text-slate-800">Page not found!</h1>
-        <p className="text-xl text-slate-800 text-center">
-          Please <strong>refresh the page</strong> (Because sometimes the page doesn&apos;t load on
-          the first visit.) or <b>use the search</b>.
-        </p>
+        <h1 className="text-3xl text-slate-800">
+          Page not found <span className="text-pink-700">but it may be my fault</span>!
+        </h1>
+        <div className="text-lg text-slate-800 flex flex-col gap-2">
+          <div className="flex items-baseline gap-2">
+            <div className="bg-pink-700 text-white p-2 rounded-full w-6 h-6 flex shrink-0 items-center justify-center">
+              1
+            </div>
+            <div>
+              Please <strong>refresh the page</strong> because sometimes the page doesn&apos;t load
+              on the first visit..
+            </div>
+          </div>
+          <div className="flex items-baseline gap-2">
+            <div className="bg-pink-700 text-white p-2 rounded-full w-6 h-6 flex shrink-0 items-center justify-center">
+              2
+            </div>
+            <div>
+              Please change from <code className="text-pink-700">/something/</code> to{' '}
+              <code className="text-pink-700">/note/something/</code> because I've changed the route
+              of all notes to <code className="font-bold">/note/</code>.
+            </div>
+          </div>
+          <div className="flex items-baseline gap-2">
+            <div className="bg-pink-700 text-white p-2 rounded-full w-6 h-6 flex shrink-0 items-center justify-center">
+              3
+            </div>
+            <div>
+              Please <strong>use search</strong>.
+            </div>
+          </div>
+        </div>
         <p className="text-xl">
           <Link className="m2it-link hover:m2it-link-hover font-semibold" href="/">
             Back to home
