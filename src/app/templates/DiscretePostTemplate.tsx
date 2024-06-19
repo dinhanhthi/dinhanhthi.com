fa10b1c648f9c19794365ba0714decfecadcad64
Thi <dinhanhthi@gmail.com>
2024-06-19 19:55:57 +0200

    chore
    chore
    
    doc: modify cv
    
    notion-x

--- src/app/templates/DiscretePostTemplate.tsx
+++ src/app/templates/DiscretePostTemplate.tsx
@@ -32,6 +32,14 @@ export default function DiscretePostTemplate(props: DiscretePostTemplateType) {
               [containerNormal]: discreteColsType === 'single'
             })}
           >
+            {props.postProps.isDraft && (
+              <div className="flex rounded-md bg-amber-100 items-center gap-2 mb-8">
+                <div className="text-2xl pl-4 py-2">⚠️</div>
+                <div className="pr-4 w-0 flex-1 text-slate-800">
+                  This is a quick &amp; dirty draft, for me only!
+                </div>
+              </div>
+            )}
             <PostBody
               recordMap={props.recordMap}
               blockOptions={{
