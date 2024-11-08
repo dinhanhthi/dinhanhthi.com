347c96adbf037a9c7ffe7512a372febfee9473d8
Thi <dinhanhthi@gmail.com>
2024-11-08 10:27:59 +0100

    refactor: remove poppins + use opensans

--- src/app/lib/fonts.ts
+++ src/app/lib/fonts.ts
@@ -1,5 +1,5 @@
 // How to use? Put Comfortaa.className inside className
-import { Comfortaa, Poppins, Questrial } from 'next/font/google'
+import { Comfortaa, Open_Sans } from 'next/font/google'
 
 export const comfortaa = Comfortaa({
   subsets: ['latin'],
@@ -7,14 +7,9 @@ export const comfortaa = Comfortaa({
   weight: ['400', '700']
 })
 
-export const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '500', '600']
-})
-
-export const questrial = Questrial({
+export const openSans = Open_Sans({
   subsets: ['latin', 'vietnamese'],
   display: 'swap',
-  weight: ['400']
+  weight: ['400', '600'],
+  variable: '--font-open-sans'
 })
