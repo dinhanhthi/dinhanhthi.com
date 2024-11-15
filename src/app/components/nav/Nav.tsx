955aec748352971894701e0296f70b6310951b32
Thi <dinhanhthi@gmail.com>
2024-11-15 07:58:32 +0100

    mobile: replace Menu by app icon

--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -16,7 +16,7 @@ const navHeight = 'h-14'
 export const paddingTopNav = 'pt-14' // Must be the same as navHeight!
 const navClasses = 'bg-nav-dark-bg shadow-transparent text-gray-300'
 export const textClass = 'text-gray-300 md:hover:text-white md:hover:bg-gray-700'
-export const groupSpaceClass = 'ml-2 md:ml-4'
+export const groupSpaceClass = 'ml-0 md:ml-4'
 
 export const navLabelClass = 'text-[0.9rem]'
 
@@ -30,7 +30,7 @@ export default function Nav() {
         <Container className={cn('h-full', containerWide)}>
           <div className="flex h-full flex-wrap items-center justify-items-stretch">
             <div className="w-full">
-              <div className="mx-auto px-4 lg:px-0">
+              <div className="mx-auto px-0">
                 <div className="relative flex items-center justify-between">
                   <nav
                     className="flex flex-1 items-center justify-center gap-2 sm:gap-0 md:justify-start"
