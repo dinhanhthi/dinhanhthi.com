1a46e5752bb872aa5b5f006cbdd172b76f8590ec
Anh-Thi DINH <dinhanhthi@gmail.com>
2025-10-05 21:37:02 +0200

    Upgrade notion-client, notion-types, notion-utils to version ~7.6.0

--- src/lib/notion/notionx.ts
+++ src/lib/notion/notionx.ts
@@ -3,25 +3,7 @@ import { ExtendedRecordMap } from 'notion-types'
 
 export const notionX = new NotionAPI({
   activeUser: process.env.NOTION_ACTIVE_USER,
-  authToken: process.env.NOTION_TOKEN_V2,
-  kyOptions: {
-    hooks: {
-      beforeRequest: [
-        (request, options) => {
-          const url = request.url.toString()
-
-          if (url.includes('/api/v3/syncRecordValues')) {
-            return new Request(
-              url.replace('/api/v3/syncRecordValues', '/api/v3/syncRecordValuesMain'),
-              options
-            )
-          }
-
-          return request
-        }
-      ]
-    }
-  }
+  authToken: process.env.NOTION_TOKEN_V2
 })
 
 export async function getPage(pageId: string) {
