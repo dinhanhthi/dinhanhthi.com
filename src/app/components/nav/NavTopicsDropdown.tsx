8b7a21e74a20d971b81768635468509ada7650e5
Anh-Thi DINH <dinhanhthi@gmail.com>
2025-10-10 23:25:33 +0200

    refactor: replace completely @headlessui by @shadcn

--- src/app/components/nav/NavTopicsDropdown.tsx
+++ src/app/components/nav/NavTopicsDropdown.tsx
@@ -1,68 +1,55 @@
 'use client'
 
-import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react'
 import cn from 'classnames'
 import Link from 'next/link'
-import { useRef } from 'react'
+import { useState } from 'react'
 
 import { HIDDEN_MENUS, MENUS } from '../../../data/menus'
 import { MaterialSymbolsApps } from '../../icons/MaterialSymbolsApps'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { menuItemCommonClass } from './NavTopicItem'
 
 const dropdownItemClass = 'flex items-center p-3 whitespace-nowrap'
 
 export default function NavTopicsDropdown() {
-  const btnRef = useRef<HTMLButtonElement>(null)
-  const onClickOpt = (open: boolean) => {
-    if (!open) return
-    if (open) btnRef.current?.click()
+  const [open, setOpen] = useState(false)
+  const onClickOpt = () => {
+    setOpen(false)
   }
   return (
     <>
-      <Popover className="relative">
-        {({ open }) => (
-          <>
-            <PopoverButton
-              ref={btnRef}
-              className={cn(
-                'group flex p-0 !outline-none lg:hidden',
-                { '!bg-slate-700 text-white': open, 'text-opacity-90': !open },
-                menuItemCommonClass,
-                'ml-2 !p-1.5'
-              )}
-            >
-              <MaterialSymbolsApps className="h-8 w-8" />
-            </PopoverButton>
-            <Transition
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <PopoverPanel className="absolute left-0 z-10 mt-4 w-fit shadow-xl sm:px-0 lg:max-w-3xl">
-                <div className="overflow-hidden rounded-lg shadow-lg">
-                  <div className="bg-nav-dark-bg relative grid divide-y divide-slate-600 px-3 py-1">
-                    {MENUS.concat(HIDDEN_MENUS).map(item => (
-                      <Link
-                        onClick={() => onClickOpt(open)}
-                        key={item.uri}
-                        href={item.uri}
-                        className={dropdownItemClass}
-                      >
-                        <div className="flex gap-2">
-                          {item.icon}
-                          {item.name}
-                        </div>
-                      </Link>
-                    ))}
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <button
+            className={cn(
+              'group flex p-0 !outline-none lg:hidden',
+              { '!bg-slate-700 text-white': open, 'text-opacity-90': !open },
+              menuItemCommonClass,
+              'ml-2 !p-1.5'
+            )}
+          >
+            <MaterialSymbolsApps className="h-8 w-8" />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent align="start" className="w-fit p-0 shadow-xl" sideOffset={16}>
+          <div className="overflow-hidden rounded-lg shadow-lg">
+            <div className="bg-nav-dark-bg relative grid divide-y divide-slate-600 px-3 py-1">
+              {MENUS.concat(HIDDEN_MENUS).map(item => (
+                <Link
+                  onClick={onClickOpt}
+                  key={item.uri}
+                  href={item.uri}
+                  className={dropdownItemClass}
+                >
+                  <div className="flex gap-2">
+                    {item.icon}
+                    {item.name}
                   </div>
-                </div>
-              </PopoverPanel>
-            </Transition>
-          </>
-        )}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </PopoverContent>
       </Popover>
     </>
   )
