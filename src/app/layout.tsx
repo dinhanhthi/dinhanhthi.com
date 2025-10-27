51de626776f3ccdeba764059bc1f66452a98c30c
Anh-Thi DINH <dinhanhthi@gmail.com>
2025-10-27 18:05:25 +0100

    Make dark theme as default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body suppressHydrationWarning={true}>
         <ThemeProvider
           attribute="data-theme"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
