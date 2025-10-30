9bc816a47ea04868309f17cf5d6e7aee096082cf
Anh-Thi DINH <dinhanhthi@gmail.com>
2025-10-30 16:39:23 +0100

    minor fixes

--- next.config.mjs
+++ next.config.mjs
@@ -60,6 +60,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '**.amazonaws.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudinary.com'
       }
     ]
   }
